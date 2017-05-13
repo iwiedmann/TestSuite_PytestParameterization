a07cff31b2886ccc715414f09d07c982571c0723
Isaac Wiedmann <iwiedmann@gmail.com>
2017-05-13 16:52:38 -0700

    Finished conftest.py.

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ pytest configuration file.
 
 import pytest
 
-from globals import MACHINES
+from globals import CONFIGURATIONS, MACHINES
 
 
 def pytest_addoption(parser):
@@ -12,10 +12,18 @@ def pytest_addoption(parser):
     Adds the machine command line option which defines which machines the tests will be run over.
     :param parser: built-in argparse pytest object
     """
-    parser.addoption("--machines", required=True, choices=MACHINES.keys(), action="append", default=[],
-                     help="input the machines the tests will be run over")
+    available_machines = MACHINES.keys()
+    parser.addoption(
+        "--machines",
+        required=True,
+        choices=available_machines,
+        action="append",
+        default=[],
+        help="Input the machines the tests will be run over.  Options: {machines}".format(machines=available_machines)
+    )
 
 
+# TODO: change to a singular "mahcine" if possible
 def pytest_generate_tests(metafunc):
     """
     Parameterize the whole test suite over the user defined machines from the command line
@@ -25,4 +33,21 @@ def pytest_generate_tests(metafunc):
         metafunc.parametrize("machines", metafunc.config.option.machines)
 
 
-
+@pytest.fixture(scope="session", autouse=True)
+def machine_setup(machines, request):
+    """
+    Global setup and teardown fixture for each machine in the parameterized list of machines to run.  All tests will
+    inherit this fixture and hence all tests will be run over all defined machines unless specified in the test.
+    :param machines: current machine to run all tests over
+    :param request: built-in pytest request object
+    """
+    # optional global machine setup
+    if MACHINES[machines] == CONFIGURATIONS["config2"]:
+        # do some global setup for config2
+        pass
+    # optional global machine teardown
+    def teardown():
+        if MACHINES[machines] == CONFIGURATIONS["config2"]:
+            # do some global teardown for config2
+            pass
+    request.addfinalizer(teardown)
