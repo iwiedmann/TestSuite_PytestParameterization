6dafec197936a1dcd774347fa516ca3a9eb825e1
Isaac Wiedmann <iwiedmann@gmail.com>
2017-05-13 15:48:24 -0700

    Added a globals file.

---
+++ globals.py
@@ -0,0 +1,17 @@
+"""
+Global test suite constants.
+"""
+
+
+# configurations
+CONFIGURATIONS = {
+    "config1": "blah blah blah",
+    "config2": "more blah blah blah"
+}
+
+# available test machines and their configuration
+MACHINES = {
+    "M1": CONFIGURATIONS["config1"],
+    "M4": CONFIGURATIONS["config2"],
+    "M9": CONFIGURATIONS["config1"]
+}
